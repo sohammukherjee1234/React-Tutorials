8a247a1245bee6e337afd2db9df0e3f09c91fe6b
sohammukherjee1234 <rockstarsam1234@gmail.com>
2020-05-17 13:31:33 +0530

    No friends display message added

--- github-cards/src/Home.js
+++ github-cards/src/Home.js
@@ -5,9 +5,19 @@ import {useSelector} from "react-redux";
 export default function Home(){
 
     const friends = useSelector(state => state.users);
+
+    const displayCards = (friends) => {
+      if(friends.length === 0)
+          return (
+              <h2>No Friends to display</h2>
+          );
+      return (
+          friends.map(user => <SmallCards name={user.name} avatar={user.avatar}/>)
+      );
+    };
     return (
       <div className="wrapper">
-          {friends.map(user => <SmallCards name={user.name} avatar={user.avatar}/>)}
+          {displayCards(friends)}
       </div>
     );
 }
